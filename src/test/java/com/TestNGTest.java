985e08661b1d9e3770b6aeaaa2544b8884fdbbf1
Yogeshjec <yogesh.jec@gmail.com>
2017-11-23 16:30:07 +0530

    Update TestNGTest.java

--- src/test/java/com/TestNGTest.java
+++ src/test/java/com/TestNGTest.java
@@ -3,6 +3,7 @@ package com;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import static org.junit.Assert.assertEquals;
 
 public class TestNGTest {
 
